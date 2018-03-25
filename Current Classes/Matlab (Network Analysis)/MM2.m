048f856b7668e2ddd50868ae8234267df4d46306
kosirjm <work@jonkosir.me>
2018-03-25 17:04:42 -0400

    Fixing switch cases

--- Current Classes/Matlab (Network Analysis)/MM2.m
+++ Current Classes/Matlab (Network Analysis)/MM2.m
@@ -6,56 +6,71 @@ ECE 461
 %}
 
 % Variables
+clear all vars
 lambda = 8;
 mu = 5;
 ro = lambda/mu;
 speed = 1;
 numPackets = 1000;
+numServers = 2;
 serverProb = 5;
-arrival = -log(rand)/lambda;       
+queueSize = 20;
+
 timerVal = tic;
 interval = 1/speed;
 ticker = interval;
-arrivalMatrix(1) = arrival;
-numProcessed = 1;
-numServers = 2;
-serverState = zereos(1:numServers);
-queues = zeros(numServers : 4);
+
+blocked = zeros(1:numServers);
+serversState = zeros(1:numServers);
+queueCurrentSize = zeros(1 : numServers);
+state = [6 0]; % [state serverNumber]
+queue = zeros(numServers,queueSize);
+packet = zeros(numPackets,4); % [Arrival processTime departure server]
+
+arrival = -log(rand)/lambda;
+packet(1) = arrival;
 for i = 2:numPackets
 arrival = -log(rand)./lambda;
-arrivalMatrix(i) = arrival + arrivalMatrix(i-1);
+packet(i,1) = arrival + packet(i-1,1);
 end
-arrivalMatrix
 
 disp('Start Sim');
-disp(['Sim will take about ', num2str(arrivalMatrix(length(arrivalMatrix))), ' Seconds']);
-state = 0;
+disp(['Sim will take about ', num2str(packet(length(packet),1)), ' Seconds']);
 
-while i <= length(arrivalMatrix)
-    
-    packet(1,1) = arrivalMatrix(i);
-    
+i = 1;
+while i <= numPackets+1
     % Arrival
-    if(arrivalMatrix(i) >= toc(timerVal))
-        i = i + 1;
+    if(packet(i,1) >= toc(timerVal))
+        
         choice = randi(10);
-        if choice <= 5
-             server = 1;
-             state = 1;
+        if choice <= serverProb
+            disp('Choise 1');
+             state = [serversState(1) 1];
+             packet(i,4) = 1;
         else
-            server = 2;
-            state = 1;
+            disp('Choice 2');
+             state = [serversState(2) 2];
+             packet(i,4) = 2;
+        end
+        i = i + 1
+    end
+    
+    % Process
+    for j = 1:numServers
+        if(packet(j,3) >= toc(timerVal))
+            state = [2 j];
         end
     end
     
+    %{
     % Departure
     for i = 1:numServers
-        if(queue(i,1) >= toc(timerVal)
-            state = 2;
-            server = i;
+        if(packet(i,3) >= toc(timerVal))
+            state = [3 i];
             break;
         end
     end
+    %}
     
     % Arrival
         % Queued ----
@@ -67,42 +82,60 @@ while i <= length(arrivalMatrix)
         % Queue
         % Depature state
         
-    switch state
+    switch state(1)
         
         case 0 %Empty state
-            
+            disp ('Case 0');
             % If empty and new packet arrives process
             processTime = exprnd(mu);
-            process(packet, processTime, timerVal)
+            packet(i,2) = toc(timerVal);
+            packet(i,3) = packet(i,2) + processTime;
+            serversState(1,state(2)) = 1;
             
-        case 1 %Queue
-            
+        case 1
             % Arrival
-            queue(packet, q, server);
-            
+            % Need to fix blocking still
+            %disp ('Case 1');
+            if queueCurrentSize(state(1,2)) >= queueSize
+                blocked(state(2)) = blocked(state(2)) + 1;
+            else
+                processTime = exprnd(mu);
+
+                if queueCurrentSize(state(1,2)) == 1
+                    packet(i,2) = packet(queue(state(2),queueCurrentSize-1));
+                    packet(i,3) = packet(i,2) + processTime;
+                else
+                    packet(i,2) = toc(timerVal);
+                    packet(i,3) = packet(i,2) + processTime;  
+                end
+            end
+   
         case 2  % Process
-            processTime = exprnd(mu);
-            process(packet, processTime, timerVal)
+            disp ('Case 2');
+            queue(state(2),1:queueCurrentSize(state(2))) = queue(state(2),2:queueCurrentSize(state(2)));
+            queue(state(2),queueCurrentSize(state(2))) = 0;
+            queueCurrentSize(state(2)) = queueCurrentSize(state(1,2)) - 1;
+            if queueCurrentSize(state(2)) == 0
+                serversState(1,state(2)) = 0;
+            end
             
-        case 3 % Depature state
             
-            server1Queue(1:end-1) = array(2:end);
-            array{end} = newvalue;
-            % Put new packet in system 
+       
+        case 6 % Depature state
+            %{
+            queueCurrentSize(state(1,2)) = queueCurrentSize(state(1,2)) - 1;
             
             
-                % set to zero state
-               % if 
-            %choose next packet
-                % Reduce queue
-                
-            % Else set at zero state
+            if QueueCurrentSize(state(1,2)) == 0
+                serversState(1,state(1,2)) = 0;
+            end
+            
+            queue(queueCurrentSize,state(1,2)) = array(2:end);
+            array{end} = 0;
+           %}
         otherwise
             
     end
     
-  
-   
-    
-    
 end
+%}
