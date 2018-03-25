a2ee7f8fed805ec9dfdf6e6d59acd4e0150f996b
kosirjm <work@jonkosir.me>
2018-03-25 14:05:15 -0400

    MM2 Updates

--- Current Classes/Matlab (Network Analysis)/MM2.m
+++ Current Classes/Matlab (Network Analysis)/MM2.m
@@ -18,8 +18,9 @@ interval = 1/speed;
 ticker = interval;
 arrivalMatrix(1) = arrival;
 numProcessed = 1;
-
-
+numServers = 2;
+serverState = zereos(1:numServers);
+queues = zeros(numServers : 4);
 for i = 2:numPackets
 arrival = -log(rand)./lambda;
 arrivalMatrix(i) = arrival + arrivalMatrix(i-1);
@@ -27,30 +28,71 @@ end
 arrivalMatrix
 
 disp('Start Sim');
-disp(['Sime will take about ' arrivalMatrix(length(ArrivalMatrix))]);
+disp(['Sim will take about ', num2str(arrivalMatrix(length(arrivalMatrix))), ' Seconds']);
+state = 0;
 
-for i = 1:length(arrivalMatrix)
+while i <= length(arrivalMatrix)
+    
     packet(1,1) = arrivalMatrix(i);
+    
+    % Arrival
+    if(arrivalMatrix(i) >= toc(timerVal))
+        i = i + 1;
+        choice = randi(10);
+        if choice <= 5
+             server = 1;
+             state = 1;
+        else
+            server = 2;
+            state = 1;
+        end
+    end
+    
+    % Departure
+    for i = 1:numServers
+        if(queue(i,1) >= toc(timerVal)
+            state = 2;
+            server = i;
+            break;
+        end
+    end
+    
+    % Arrival
+        % Queued ----
+        % Process 
+    % Departure
+        % Take from queue
+        % Zero State
+    % Processing
+        % Queue
+        % Depature state
+        
     switch state
         
         case 0 %Empty state
             
-        case 1 % Queue state
-            % Arrival
+            % If empty and new packet arrives process
+            processTime = exprnd(mu);
+            process(packet, processTime, timerVal)
             
-            choice = randi(10)
-            if choice <= 5
-             queue(packet, q, server1Queue);
-            else
-             queue(packet, q, server2Queue);
-            end
-           
-        case 2  % Process/Depature state
-           
+        case 1 %Queue
+            
+            % Arrival
+            queue(packet, q, server);
+            
+        case 2  % Process
+            processTime = exprnd(mu);
+            process(packet, processTime, timerVal)
+            
+        case 3 % Depature state
+            
+            server1Queue(1:end-1) = array(2:end);
+            array{end} = newvalue;
             % Put new packet in system 
             
-            % If queueu == 1
+            
                 % set to zero state
+               % if 
             %choose next packet
                 % Reduce queue
                 
@@ -59,7 +101,7 @@ for i = 1:length(arrivalMatrix)
             
     end
     
-    
+  
    
     
     
--- Current Classes/Matlab (Network Analysis)/process.m
+++ Current Classes/Matlab (Network Analysis)/process.m
@@ -1,7 +1,13 @@
-function [ output_args ] = process( input_args )
+function packet = process(packet, processTime, timerVal)
 %PROCESS Summary of this function goes here
 %   Detailed explanation goes here
+% Packet (1,1) = ArrivalTime
+% Packet (1,2) = Start Process Time
+% Packet (1,3) = Departure Time
 
+packet(1,2) = toc(timerVal);
+packet(1,3) = toc(timerVal) + processTime;
 
+packet;
 end
 
